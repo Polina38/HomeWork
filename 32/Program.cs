d20e6a82db18e229ba82189354768239d3da0b6c
Polina <sunlight_polina@mail.ru>
2022-02-05 16:36:31 +0800

    32

---
+++ 32/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задать массив из 8 элементов, заполненных нулями и единицами вывести их на экран
+
+int[] array = new int[8];
+Random rand = new Random();
+
+
+for (int i = 0; i < 8; i++) 
+{
+    array[i]=rand.Next(0,2);
+    System.Console.WriteLine($"{array[i]}");
+}
+
