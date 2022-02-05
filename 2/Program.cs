9720350c710a55d63158337f92cf0fb73cc4e5e8
Polina <sunlight_polina@mail.ru>
2022-02-05 14:51:10 +0800

    2

---
+++ 2/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int a=3, b=1;
+if (a>b) System.Console.WriteLine("a больше b");
+if (a<b) System.Console.WriteLine("a меньше b");
+if (a==b) Console.WriteLine("a равно b");
