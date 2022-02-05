21435e58e491addd883695b1b273e9b0545121c3
Polina <sunlight_polina@mail.ru>
2022-02-05 23:15:31 +0800

    31

--- 31/Program.cs
+++ 31/Program.cs
@@ -1,10 +1,12 @@
 ﻿// создать массив из 8 элементов
 
-int [] array= new int [8];
+int[] array= new int [8];
 Random range= new Random();
 
 for (int i=0; i<8; i++)
 {
-    array[i] = Random.Next(0,100);
-    Console.WriteLine(array[i]);
+    array[i] = range.Next(0,100);
+    Console.Write(array[i]+" ");
 }
+
+
