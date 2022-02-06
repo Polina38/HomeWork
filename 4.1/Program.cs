e9c7991d86de8a49de030dcf392ac36dca89f001
Polina <sunlight_polina@mail.ru>
2022-02-06 22:33:23 +0800

    4.1

---
+++ 4.1/Program.cs
@@ -0,0 +1,17 @@
+﻿// По заданному номеру дня недели вывести его название
+
+Console.Clear();
+System.Console.WriteLine("Введите номер дня недели от 1 до 7");
+string d;
+int a;
+
+d = Console.ReadLine();
+a = (int)Convert.ToInt32(d);
+
+if (a == 1) System.Console.WriteLine("Это понедельник :((");
+if (a == 2) System.Console.WriteLine("Это вторник :(");
+if (a == 3) System.Console.WriteLine("Это среда :|");
+if (a == 4) System.Console.WriteLine("Это четверг :)");
+if (a == 5) System.Console.WriteLine("Это пятница :))");
+if (a == 6) System.Console.WriteLine("Это суббота! УРА!");
+if (a == 7) System.Console.WriteLine("Это воскресенье!");
