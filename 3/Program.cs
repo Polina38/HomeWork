740924f1868c1981d4b5490f8fc15fc17c2380a9
Polina <sunlight_polina@mail.ru>
2022-02-06 22:05:35 +0800

    3

--- 3/Program.cs
+++ 3/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Даны два числа. Показать большее и меньшее число
+
+Console.Clear();
+string a, b;
+int c, d;
+Console.WriteLine("Введите первое число");
+a = Console.ReadLine();
+c = Convert.ToInt32(a);
+System.Console.WriteLine("Введите второе число");
+b = Console.ReadLine();
+d = Convert.ToInt32(b);
+if (c > d) System.Console.WriteLine("Большее число " + c + " " + "Меньшее число " + d);
+else System.Console.WriteLine("Большее число " + d + " " + "Меньшее число " + c);
