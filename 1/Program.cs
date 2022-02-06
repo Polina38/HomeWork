3a472a9883087c034ade4257ccb42d420de9d0c3
Polina <sunlight_polina@mail.ru>
2022-02-06 21:50:31 +0800

    change

--- 1/Program.cs
+++ 1/Program.cs
@@ -1,13 +1,13 @@
 ﻿// Вывести квадрат числа
 
 Console.Clear();
-int c;
-double b;
+int c,b;
+
 string a;
 Console.WriteLine("Введите любое целое число");
 a = Console.ReadLine();
 c = Convert.ToInt32(a);
-b = Math.Pow(c, 2);
+b = (int)Math.Pow(c, 2);
 
 Console.WriteLine("Квадрат Вашего числа равен ");
 Console.Write(b);
--- 2.1/Program.cs
+++ 2.1/Program.cs
@@ -1,9 +1,10 @@
 ﻿// По двум заданным числам проверять является ли первое квадратом второго
-int a=149, b=7;
-int square = b*b;
+
+int a = 49, b = 7;
+int square = b * b;
 // int square2= (int) Math.Pow(b,2);
-if (a==square) 
+if (a == square)
 {
-   Console.WriteLine("a квадрат b"); 
+    Console.WriteLine("a является квадратом b");
 }
 else Console.WriteLine("a не является квадратом b");
