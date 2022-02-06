289797ee547da5434f4eb780f08be394022c0f17
Polina <sunlight_polina@mail.ru>
2022-02-06 23:59:54 +0800

    some

---
+++ 7/Program.cs
@@ -0,0 +1,11 @@
+﻿// Выяснить является ли число чётным
+
+Console.Clear();
+System.Console.WriteLine("Введите целое число");
+String s;
+int a;
+s=Console.ReadLine();
+a=Convert.ToInt32(s);
+
+if (a%2==0) System.Console.WriteLine("Ваше число четное");
+else Console.WriteLine("Ваше число нечетное");
