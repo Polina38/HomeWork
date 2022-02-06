cd1f4899b0d982e9b114c0cda163a0b5e59703f8
Polina <sunlight_polina@mail.ru>
2022-02-06 23:38:50 +0800

    6

---
+++ 5/Program.cs
@@ -0,0 +1,25 @@
+﻿// Найти максимальное из трех чисел
+
+Console.Clear();
+System.Console.WriteLine("Введите первое число");
+string a1,a2,a3;
+int b1,b2,b3,max;
+
+a1 = Console.ReadLine();
+b1 = (int)Convert.ToInt32(a1);
+
+System.Console.WriteLine("Введите второе число");
+
+a2 = Console.ReadLine();
+b2 = (int)Convert.ToInt32(a2);
+
+System.Console.WriteLine("Введите третье число");
+
+a3 = Console.ReadLine();
+b3 = (int)Convert.ToInt32(a3);
+
+max=b1;
+if (b2>max) max=b2;
+if (b3>max) max=b3;
+
+Console.WriteLine("Максимальное число "+ max);
--- 6/Program.cs
+++ 6/Program.cs
@@ -1,10 +1,15 @@
-﻿// 
+﻿// Написать программу вычисления значения функции y = f(a)
 
 void Pause (string message)
 {
     Console.WriteLine(message);
     Console.ReadKey();
-
 }
-
    Pause("Press any key");
+
+double F(double a)
+{
+    return a*3;
+}
+System.Console.WriteLine(F(50));
+
