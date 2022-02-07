33e8fe7d4cfa7ce279e7921526ac51d30458b5d0
Polina <sunlight_polina@mail.ru>
2022-02-08 07:29:24 +0800

    8

---
+++ 8/Program.cs
@@ -0,0 +1,18 @@
+﻿// Показать числа от -N до N
+
+
+Console.Clear();
+Console.WriteLine("Введите любое число N");
+string a;
+int b;
+a=Console.ReadLine();
+b=Convert.ToInt32(a);
+int [] array= new int [2*b+1];
+
+
+for (int i=0;i<array.Length;i++ )
+{
+    array[i]=(-b+i);
+    System.Console.Write(array[i]+" ");
+}
+
