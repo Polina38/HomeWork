fda6fb292f931385a5205c1563beda50e854213c
Polina <sunlight_polina@mail.ru>
2022-02-08 07:10:22 +0800

    9

---
+++ 9/Program.cs
@@ -0,0 +1,16 @@
+﻿// Показать четные числа от 1 до N
+
+Console.Clear();
+Console.WriteLine("Введите любое число N");
+string a;
+int b;
+a=Console.ReadLine();
+b=Convert.ToInt32(a);
+int [] array= new int [b+1];
+
+System.Console.WriteLine("Все четные числа до указанного Вами числа ");
+for (int i=1;i<array.Length;i++ )
+{
+    array[i]=i;
+    if (array[i]%2==0) System.Console.Write(array[i]+" ");
+}
