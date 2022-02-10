afaba417765130230ecffd656fe92b622374cad6
Polina <sunlight_polina@mail.ru>
2022-02-10 22:33:45 +0800

    connect

---
+++ 10/Program.cs
@@ -0,0 +1,9 @@
+﻿// Показать последнюю цифру трёхзначного числа
+Console.Clear();
+
+Console.WriteLine("Введите любое трехзначное число");
+string a= Console.ReadLine();
+int b, c;
+b= Convert.ToInt32(a);
+c= b%10;
+System.Console.WriteLine(c);
---
+++ 11/Program.cs
@@ -0,0 +1,11 @@
+﻿// Показать вторую цифру трёхзначного числа
+
+Console.Clear();
+
+Console.WriteLine("Введите любое трехзначное число");
+string a= Console.ReadLine();
+int b, c, d;
+b= Convert.ToInt32(a);
+c= b%100;
+d=c/10;
+System.Console.WriteLine(d);
