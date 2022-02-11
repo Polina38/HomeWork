db0500166774c8bd9089ad553d7f90ae248c6a97
Polina <sunlight_polina@mail.ru>
2022-02-12 03:13:59 +0800

    12

---
+++ 12/Program.cs
@@ -0,0 +1,21 @@
+﻿// Дано число из отрезка [10, 99]. Показать наибольшую цифру числа
+
+Console.Clear ();
+int a,b,c, max;
+
+Random Rang= new Random ();
+
+a= Rang.Next(10,99);
+Console.WriteLine("Первое число "+a);
+
+b= Rang.Next(10,99);
+Console.WriteLine("Первое число "+b);
+
+c= Rang.Next(10,99);
+Console.WriteLine("Первое число "+c);
+
+max=a;
+if (b>max) max=b;
+if (c>max) max=c;
+
+System.Console.WriteLine("Наибольшее число "+max);
