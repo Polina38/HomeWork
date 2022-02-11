705b6d7aab240cd2754164a3f111a76a2df3d1b5
Polina <sunlight_polina@mail.ru>
2022-02-12 03:45:51 +0800

    14

---
+++ 14/Program.cs
@@ -0,0 +1,14 @@
+﻿// Выяснить, кратно ли число заданному, если нет, вывести остаток.
+
+Console.Clear();
+int a, b, d;
+Console.WriteLine("Введите заданное число");
+string s = Console.ReadLine();
+a = Convert.ToInt32(s);
+
+Console.WriteLine("Введите любое число");
+string c = Console.ReadLine();
+b = Convert.ToInt32(c);
+
+if (b % a == 0) System.Console.WriteLine("Ваше число кратно заданному");
+else System.Console.WriteLine("Остаток от деления составляет " + b % a);
