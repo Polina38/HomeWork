6cae3df88d0bbb4683db7b2088d9ffce335a67ae
Polina <sunlight_polina@mail.ru>
2022-02-12 03:14:34 +0800

    13

---
+++ 13/Program.cs
@@ -0,0 +1,12 @@
+﻿// Удалить вторую цифру трёхзначного числа
+
+
+Console.Clear();
+
+Console.WriteLine("Введите любое трехзначное число");
+string a= Console.ReadLine();
+int b, c, d;
+b= Convert.ToInt32(a);
+c= b%10;
+d=b/100;
+System.Console.WriteLine(d+" "+c);
