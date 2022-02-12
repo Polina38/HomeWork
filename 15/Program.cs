bbbe854cd88ba23e18c159098d751bbafcab2bbe
Polina <sunlight_polina@mail.ru>
2022-02-13 02:38:24 +0800

    15

---
+++ 15/Program.cs
@@ -0,0 +1,19 @@
+﻿// Найти третью цифру числа или сообщить, что её нет
+
+Console.Clear();
+
+System.Console.WriteLine("Введите любое число");
+
+string s = Console.ReadLine();
+string s1= String.Empty;
+if (s.Length < 3)
+{
+    System.Console.WriteLine("У Вашего числа нет третьей цифры");
+}
+else
+{
+    for (int i = 0; i < s.Length; i++)
+    {
+        if (i == 2) System.Console.WriteLine("Третье цифра в числе "+s[i]);
+    }
+}
