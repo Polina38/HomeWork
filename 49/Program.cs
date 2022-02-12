8fb2dcab19a6aaf60710c0a6b33262306b0b037a
Polina <sunlight_polina@mail.ru>
2022-02-13 04:34:45 +0800

    49

---
+++ 49/Program.cs
@@ -0,0 +1,24 @@
+﻿//Показать двумерный массив размером m×n заполненный вещественными числами
+
+int n,m;
+string m1,n1;
+Console.WriteLine("Введите количество строк массива");
+m1 = Console.ReadLine();
+m= Convert.ToInt32(m1);
+Console.WriteLine("Введите количество столбцов массива");
+n1 = Console.ReadLine();
+n= Convert.ToInt32(n1);
+
+
+double [,] table = new double [m,n];
+
+for (int i=0; i<table.GetLength(0); i++)
+{
+    for (int j=0; j<table.GetLength(1); j++)
+    {
+        table [i,j]= new Random().NextDouble();
+        System.Console.Write(table[i,j]+" ");
+    }
+    System.Console.WriteLine();
+}
+
