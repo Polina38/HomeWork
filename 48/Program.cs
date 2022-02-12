2377cc47e8ec7fdcae1a00c96390a2b31d62fdd8
Polina <sunlight_polina@mail.ru>
2022-02-13 04:04:38 +0800

    48

---
+++ 48/Program.cs
@@ -0,0 +1,25 @@
+﻿// Показать двумерный массив размером m×n заполненный целыми числами
+
+int n,m;
+string m1,n1;
+Console.WriteLine("Введите количество строк массива");
+m1 = Console.ReadLine();
+m= (int) Convert.ToInt32(m1);
+Console.WriteLine("Введите количество столбцов массива");
+n1 = Console.ReadLine();
+n= (int) Convert.ToInt32(n1);
+
+int [,] table = new int [m,n];
+// void FillArray (int [,] table)
+// {
+for (int i=0; i<table.GetLength(0); i++)
+{
+    for (int j=0; j<table.GetLength(1); j++)
+    {
+        table [i,j]= new Random ().Next(1,10);
+        System.Console.Write(table[i,j]+" ");
+    }
+    System.Console.WriteLine();
+}
+
+// FillArray(table);
