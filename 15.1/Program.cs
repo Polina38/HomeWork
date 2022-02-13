e89fcfcdaffcf0d217cc5a34a549437590ff3636
Polina <sunlight_polina@mail.ru>
2022-02-14 00:15:07 +0800

    15.1

---
+++ 15.1/Program.cs
@@ -0,0 +1,13 @@
+﻿// Дано число. Проверить кратно ли оно 7 и 23
+
+Console.WriteLine("Введите число");
+int c;
+string s;
+s= Console.ReadLine();
+c= Convert.ToInt32(s);
+
+if (c%7!=0) System.Console.WriteLine("Число не кратно 7");
+else System.Console.WriteLine("Число кратно 7");
+
+if (c%23!=0) System.Console.WriteLine("Число не кратно 23");
+else System.Console.WriteLine("Число кратно 23");
