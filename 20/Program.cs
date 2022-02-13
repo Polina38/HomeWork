312e3ab5b8808440806693da8d3799947dfde08d
Polina <sunlight_polina@mail.ru>
2022-02-14 01:23:17 +0800

    20

---
+++ 20/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задать номер четверти, показать диапазоны для возможных координат
+
+Console.Clear();
+Console.WriteLine("Введите номер четверти декартовой системы координат");
+int a;
+string b;
+b = Console.ReadLine();
+a = Convert.ToInt32(b);
+
+if (a==1) Console.WriteLine("Для этой четверти Х>0 и Y>0");
+if (a==2) Console.WriteLine("Для этой четверти Х<0 и Y>0");
+if (a==3) Console.WriteLine("Для этой четверти Х<0 и Y<0");
+if (a==4) Console.WriteLine("Для этой четверти Х>0 и Y<0");
