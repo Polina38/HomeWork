d774a00f438b3a3ca773f5b33d88c4a8092a9bd4
Polina <sunlight_polina@mail.ru>
2022-02-14 02:10:59 +0800

    21

---
+++ 21/Program.cs
@@ -0,0 +1,16 @@
+﻿// Программа проверяет пятизначное число на палиндромом.
+
+Console.Clear();
+Console.WriteLine("Введите пятизначное число");
+string s = Console.ReadLine();
+if (s.Length != 5)
+{
+    System.Console.WriteLine("Число не из пяти знаков, ошибка");
+    return;
+}
+else
+{
+    if (s[0] == s[4] && s[1] == s[3]) System.Console.WriteLine("Число является палиндромом");
+    else Console.WriteLine("Число не является палиндромом");
+    if (s.Length != 5) System.Console.WriteLine("Число не из пяти знаков, ошибка");
+}
