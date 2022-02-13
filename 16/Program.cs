5b4ecffdb46b7a23e01fc0eeacafce56381e9a59
Polina <sunlight_polina@mail.ru>
2022-02-14 00:21:57 +0800

    16

---
+++ 16/Program.cs
@@ -0,0 +1,11 @@
+﻿// Дано число обозначающее день недели. Выяснить является номер дня недели выходным
+
+Console.WriteLine("Введите номер дня недели");
+int a;
+string b;
+b= Console.ReadLine();
+a=Convert.ToInt32(b);
+
+if (a==6 || a==7) System.Console.WriteLine("Ура, выходной!");
+else System.Console.WriteLine("Это не выходной :(");
+
