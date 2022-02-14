b70e00154b5cf9e1e9a838d627220ff1e5fa1d1e
Polina <sunlight_polina@mail.ru>
2022-02-15 02:19:56 +0800

    27

---
+++ 27/Program.cs
@@ -0,0 +1,14 @@
+﻿// Определить количество цифр в числе
+
+Console.Clear();
+System.Console.WriteLine("Введите любое число");
+string s;
+int count=0;
+s= Console.ReadLine();
+for (int i=0; i<s.Length; i++)
+{
+    count= count+1;
+}
+
+
+Console.WriteLine("В вашем числе цифр "+ count);
