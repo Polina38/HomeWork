d447d762d3ec5c156f2c7f6728c9a7c9fef14b59
Polina <sunlight_polina@mail.ru>
2022-02-15 01:54:25 +0800

    24

---
+++ 24/Program.cs
@@ -0,0 +1,17 @@
+﻿// Найти кубы чисел от 1 до N
+
+int n;
+double result;
+Console.WriteLine("Введите число N");
+n = Int32.Parse(Console.ReadLine());
+System.Console.WriteLine("Таблица кубов чисеот от 1 до " + n);
+
+for (int i = 1; i <= n; i++)
+{
+    System.Console.Write(i + " = ");
+    result = Math.Pow(i, 3);
+    System.Console.Write(result + "; ");
+
+}
+
+
