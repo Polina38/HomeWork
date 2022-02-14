0a664a4b0a721a9143b337080fbc7088d7cfab31
Polina <sunlight_polina@mail.ru>
2022-02-15 01:48:10 +0800

    23

---
+++ 23/Program.cs
@@ -0,0 +1,15 @@
+﻿// Показать таблицу квадратов чисел от 1 до N
+int n, result;
+Console.WriteLine("Введите число N");
+n = Int32.Parse(Console.ReadLine());
+
+for (int i = 1; i <= n; i++)
+{
+        System.Console.Write(i+" = ");
+        result = i * i;
+        System.Console.Write(result+"; ");
+         
+}
+
+
+
