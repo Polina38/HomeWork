ddb474854e3979334fdb6e4c9d086c90bb738154
Polina <sunlight_polina@mail.ru>
2022-02-15 02:12:15 +0800

    26

---
+++ 26/Program.cs
@@ -0,0 +1,21 @@
+﻿// Возведите число А в натуральную степень B используя цикл
+
+
+Console.Clear();
+int a, b;
+int result = 1;
+Console.WriteLine("Введите число A");
+a = Int32.Parse(Console.ReadLine());
+
+
+Console.WriteLine("Введите число В");
+b = Int32.Parse(Console.ReadLine());
+
+for (int i = 1; i <= b; i++)
+{
+    result = result * a;
+}
+Console.WriteLine("Число " + a + " в степени " + b + " равно " + result);
+
+
+
