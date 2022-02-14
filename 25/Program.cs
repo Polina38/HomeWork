f4a7276016da0406c3d1fd393413ffb34546689f
Polina <sunlight_polina@mail.ru>
2022-02-15 02:03:03 +0800

    25

---
+++ 25/Program.cs
@@ -0,0 +1,13 @@
+﻿// Найти сумму чисел от 1 до А
+
+Console.Clear();
+int a;
+int result = 0;
+Console.WriteLine("Введите число A");
+a = Int32.Parse(Console.ReadLine());
+
+for (int i = 1; i <= a; i++)
+{
+    result = result + i;
+}
+Console.WriteLine("Сумма чисел от 1 до " + a + " равна " + result);
