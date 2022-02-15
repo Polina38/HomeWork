6ffe50fb7f864a161e0a12b50741ab0679a607a5
Polina <sunlight_polina@mail.ru>
2022-02-16 01:39:02 +0800

    28

---
+++ 28/Program.cs
@@ -0,0 +1,21 @@
+﻿// Подсчитать сумму цифр в числе
+
+
+Console.Clear();
+System.Console.WriteLine("Введите любое число");
+string s;
+int b, c;
+int count = 0;
+s = Console.ReadLine();
+b = Convert.ToInt32(s);
+
+while (b > 0)
+{
+    c = b % 10;
+    count = count + c;
+    b = b / 10;
+}
+
+Console.WriteLine("Сумма чисел в вашем числе равна " + count);
+
+
