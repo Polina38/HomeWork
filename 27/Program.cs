7a08ec3b891207c0b093b55abf2e650684594502
Polina <sunlight_polina@mail.ru>
2022-02-16 02:10:04 +0800

    27.1

--- 27/Program.cs
+++ 27/Program.cs
@@ -5,10 +5,7 @@ System.Console.WriteLine("Введите любое число");
 string s;
 int count=0;
 s= Console.ReadLine();
-for (int i=0; i<s.Length; i++)
-{
-    count= count+1;
-}
+count = s.Length;
 
 
 Console.WriteLine("В вашем числе цифр "+ count);
