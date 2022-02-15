5bdc1e316e921040e06bbe2fb752c902614b29af
Polina <sunlight_polina@mail.ru>
2022-02-16 03:17:07 +0800

    30

--- 30/Program.cs
+++ 30/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Показать кубы чисел, заканчивающихся на четную цифру
+
+
+Console.Clear();
+int a;
+double b;
+double result = 1;
+Console.WriteLine("Введите любое число, до которого будет осуществляться проверка");
+a = Int32.Parse(Console.ReadLine());
+
+for (int i = 1; i <= a; i++)
+{
+    b = Math.Pow(i, 3);
+    if (b % 2 == 0) System.Console.WriteLine(i + " куб этого числа четный " + b);
+
+}
+System.Console.WriteLine();
+
