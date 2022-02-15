a3cecd73c8eb81b081e304326ddc32ff254a506d
Polina <sunlight_polina@mail.ru>
2022-02-16 02:05:10 +0800

    29

---
+++ 29/Program.cs
@@ -0,0 +1,16 @@
+﻿// Написать программу вычисления произведения чисел от 1 до N
+
+
+Console.Clear();
+int a;
+double result = 1;
+Console.WriteLine("Введите любое число N");
+a = Int32.Parse(Console.ReadLine());
+
+for (int i = 1; i <= a; i++)
+{
+    result = result * i;
+}
+Console.WriteLine("Произведение чисел от 1 до " + a + " равна " + result);
+
+
