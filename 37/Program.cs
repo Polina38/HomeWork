34b1579a0576e1327fe2678a457953870418699f
Polina <sunlight_polina@mail.ru>
2022-02-17 02:28:25 +0800

    37

---
+++ 37/Program.cs
@@ -0,0 +1,19 @@
+﻿// В одномерном массиве из 123 чисел найти количество элементов из отрезка [10,99]
+
+
+Console.Clear();
+
+int[] arr = new int[123];
+Random rang = new Random();
+int count = 0;
+
+for (int i = 0; i < arr.Length; i++)
+{
+    arr[i] = rang.Next(-99, 200);
+    System.Console.Write($"{arr[i],5}");
+    if (arr[i] < 99 && arr[i] >= 10) count = count + 1;
+}
+System.Console.WriteLine();
+System.Console.WriteLine("Количество элементов из отезка [10,99] равно " + count);
+
+
