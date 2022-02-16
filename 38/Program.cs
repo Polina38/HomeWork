7ccdffdb73942b373aae18c91f7bfa7b85d85331
Polina <sunlight_polina@mail.ru>
2022-02-17 02:43:12 +0800

    38

---
+++ 38/Program.cs
@@ -0,0 +1,16 @@
+﻿// Найти сумму чисел одномерного массива стоящих на нечетной позиции
+
+Console.Clear();
+
+int[] arr = new int[10];
+Random rang = new Random();
+int count = 0;
+
+for (int i = 0; i < arr.Length; i++)
+{
+    arr[i] = rang.Next(0, 100);
+    System.Console.Write($"{arr[i],5}");
+    if (i % 2 != 0) count = count + arr[i];
+}
+System.Console.WriteLine();
+Console.WriteLine("Сумма чисел, стоящих на нечетных позициях массива, равна " + count);
