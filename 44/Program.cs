29a119efc4acd9e4e0db7d4990e1db099aa2b2c1
Polina <sunlight_polina@mail.ru>
2022-02-20 00:34:43 +0800

    44

---
+++ 44/Program.cs
@@ -0,0 +1,16 @@
+﻿// Найти точку пересечения двух прямых заданных уравнением 
+// y = k1 * x + b1, y = k2 * x + b2, b1 k1 и b2 и k2 заданы
+
+Console.Clear();
+
+void PointAcross (int b1, int k1,int b2,int k2)
+{
+int x= (b2-b1)/(k1-k2);
+int y= x*k2+b2;
+System.Console.WriteLine("Точка пересечения прямых "+" x= "+ x +" y= "+y);
+
+}
+PointAcross(1,5,7,4);
+
+
+
