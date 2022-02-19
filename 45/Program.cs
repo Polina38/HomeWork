94f4c5074ce6448bec0d7a08ea58fd6cb0f86ca7
Polina <sunlight_polina@mail.ru>
2022-02-20 02:45:47 +0800

    45

---
+++ 45/Program.cs
@@ -0,0 +1,14 @@
+﻿// Показать числа Фибоначчи
+
+Console.Clear();
+System.Console.WriteLine("Введите порядковый номер числа, до которого будет показан ряд Фибоначчи");
+int s = Int32.Parse(Console.ReadLine());
+System.Console.Write("0 " + " 1 ");
+int[] Array = new int[s];
+for (int i = 2; i < Array.Length; i++)
+{
+    Array[0] = 0;
+    Array[1] = 1;
+    Array[i] = Array[i - 1] + Array[i - 2];
+    System.Console.Write($"{Array[i],4}");
+}
