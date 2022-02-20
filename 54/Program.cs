fae8d83b20f65f4df097a6b6f5305b76b13f3bdf
Polina <sunlight_polina@mail.ru>
2022-02-20 15:16:45 +0800

    54

---
+++ 54/Program.cs
@@ -0,0 +1,34 @@
+﻿// В матрице чисел найти сумму элементов главной диагонали
+
+Console.Clear();
+
+System.Console.WriteLine("Введите количество строк массива");
+int n = Int32.Parse(Console.ReadLine());
+System.Console.WriteLine("Введите количество столбцов массива");
+int m = Int32.Parse(Console.ReadLine());
+int[,] Array = new int[n, m];
+
+for (int i = 0; i < Array.GetLength(0); i++)
+{
+    for (int j = 0; j < Array.GetLength(1); j++)
+    {
+        Array[i, j] = new Random().Next(1, 10);
+        System.Console.Write($"{Array[i, j],3}");
+    }
+    System.Console.WriteLine();
+}
+System.Console.WriteLine();
+int count=0;
+for (int i = 0; i < Array.GetLength(0); i++)
+{
+    for (int j = 0; j < Array.GetLength(1); j++)
+    {
+        if (i == j)
+        {
+            count=count+Array[i, j];
+            
+        }
+    }
+    
+}
+Console.WriteLine("Cумма элементов основной вертикали равна "+ count);
