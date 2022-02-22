c5d4661c47986de6802710546bf35e18954c3cec
Polina <sunlight_polina@mail.ru>
2022-02-23 00:56:12 +0800

    47

---
+++ 47/Program.cs
@@ -0,0 +1,58 @@
+﻿// Написать программу копирования массива
+
+Console.Clear();
+Input(out int x, out int y);
+int[,] Matrix = new int[x, y];
+
+FillArray(Matrix);
+PrintArray(Matrix);
+
+void Input(out int n, out int m)
+{
+    System.Console.WriteLine("Введите количество строк массива");
+    n = Int32.Parse(Console.ReadLine());
+    System.Console.WriteLine("Введите количество столбцов массива");
+    m = Int32.Parse(Console.ReadLine());
+}
+
+void FillArray(int[,] Array)
+{
+    for (int i = 0; i < Array.GetLength(0); i++)
+    {
+        for (int j = 0; j < Array.GetLength(1); j++)
+        {
+            Array[i, j] = new Random().Next(1, 10);
+
+        }
+    }
+
+}
+
+void PrintArray(int[,] Array)
+{
+    for (int i = 0; i < Array.GetLength(0); i++)
+    {
+        for (int j = 0; j < Array.GetLength(1); j++)
+        {
+            System.Console.Write($"{Array[i, j],3}");
+        }
+        System.Console.WriteLine();
+    }
+    System.Console.WriteLine();
+
+    int[,] Arr = new int[x, y];
+
+    for (int i = 0; i < Arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < Arr.GetLength(1); j++)
+        {
+            Arr[i, j] = Array[i, j];
+            System.Console.Write($"{Arr[i, j],3}");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+
+
+
