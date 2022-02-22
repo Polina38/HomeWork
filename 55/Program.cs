87b387499afb070874211a5862e1139a49653ba1
Polina <sunlight_polina@mail.ru>
2022-02-23 00:03:46 +0800

    55

---
+++ 55/Program.cs
@@ -0,0 +1,62 @@
+﻿// Дан целочисленный массив. Найти среднее арифметическое каждого из столбцов.
+
+Console.Clear();
+Input(out int x, out int y);
+int[,] Matrix = new int[x, y];
+FillArray(Matrix);
+PrintArray(Matrix);
+Task(Matrix);
+
+void Input(out int n, out int m)
+{
+    System.Console.WriteLine("Введите количество строк массива");
+    n = Int32.Parse(Console.ReadLine());
+    System.Console.WriteLine("Введите количество столбцов массива");
+    m = Int32.Parse(Console.ReadLine());
+}
+
+void FillArray(int[,] Array)
+{
+    for (int i = 0; i < Array.GetLength(0); i++)
+    {
+        for (int j = 0; j < Array.GetLength(1); j++)
+        {
+            Array[i, j] = new Random().Next(1, 10);
+
+        }
+    }
+
+}
+
+void PrintArray(int[,] Array)
+{
+    for (int i = 0; i < Array.GetLength(0); i++)
+    {
+        for (int j = 0; j < Array.GetLength(1); j++)
+        {
+            System.Console.Write($"{Array[i, j],3}");
+        }
+        System.Console.WriteLine();
+    }
+    System.Console.WriteLine();
+}
+
+void Task(int[,] Array)
+{
+    int sum = 0;
+
+    for (int j = 0; j < Array.GetLength(1); j++)
+    {
+        for (int i = 0; i < Array.GetLength(0); i++)
+        {
+
+            sum = Array[i, j] + sum;
+
+        }
+        System.Console.Write($"{sum / Array.GetLength(0),3}");
+        sum = 0;
+    }
+}
+
+
+
