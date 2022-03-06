38eba31cc01e326ee401c3024f0d582010812e62
Polina <sunlight_polina@mail.ru>
2022-03-06 23:25:37 +0800

    67

---
+++ 67/Program.cs
@@ -0,0 +1,25 @@
+﻿// Показать натуральные числа от N до 1, N задано
+
+
+Console.Clear();
+Input(out int x);
+Recurs(x);
+
+
+void Input(out int n)
+{
+    System.Console.WriteLine("Введите число N");
+    n = Int32.Parse(Console.ReadLine());
+    
+}
+
+int Recurs(int a)
+{
+    
+    System.Console.Write($"{a} ");
+    if (a <= 1) return 1;
+    return Recurs(a - 1);
+}
+
+
+
