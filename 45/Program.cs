281d6d64cc56bfedb890d868a0316d381ed2e7c5
Polina <sunlight_polina@mail.ru>
2022-03-07 16:40:05 +0800

    45

--- 45/Program.cs
+++ 45/Program.cs
@@ -3,12 +3,21 @@
 Console.Clear();
 System.Console.WriteLine("Введите порядковый номер числа, до которого будет показан ряд Фибоначчи");
 int s = Int32.Parse(Console.ReadLine());
-System.Console.Write("0 " + " 1 ");
 int[] Array = new int[s];
-for (int i = 2; i < Array.Length; i++)
+for (int i = 0; i < Array.Length; i++)
 {
-    Array[0] = 0;
-    Array[1] = 1;
-    Array[i] = Array[i - 1] + Array[i - 2];
+    if (i == 0)
+    {
+        Array[i] = 0;
+    }
+    if (i == 1)
+    {
+        Array[i] = 1;
+    }
+    if (i >= 2)
+    {
+        Array[i] = Array[i - 1] + Array[i - 2];
+    }
     System.Console.Write($"{Array[i],4}");
+
 }
